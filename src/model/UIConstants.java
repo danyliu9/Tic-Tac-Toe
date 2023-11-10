2a4084411f1f787232ae76d217c36568453cbe3c
Daniel Liu <daniel.yd.liu@gmail.com>
2023-11-09 18:37:18 -0800

    Add dialog panel height/width constants

--- src/model/UIConstants.java
+++ src/model/UIConstants.java
@@ -3,4 +3,6 @@ package model;
 public class UIConstants {
     public static final int DIALOG_BUTTON_HEIGHT= 30;
     public static final int DIALOG_BUTTON_WIDTH = 150;
+    public static final int DIALOG_PANEL_HEIGHT= 100;
+    public static final int DIALOG_PANEL_WIDTH = 300;
 }
