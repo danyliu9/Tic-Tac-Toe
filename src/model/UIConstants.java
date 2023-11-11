07b8949629c766c4b32fe8df71bac32da522a476
Daniel Liu <daniel.yd.liu@gmail.com>
2023-11-11 04:58:57 -0800

    Add board UI constants

--- src/model/UIConstants.java
+++ src/model/UIConstants.java
@@ -5,4 +5,7 @@ public class UIConstants {
     public static final int DIALOG_BUTTON_WIDTH = 150;
     public static final int DIALOG_PANEL_HEIGHT= 100;
     public static final int DIALOG_PANEL_WIDTH = 300;
+
+    public static final int BOARD_HEIGHT = 300;
+    public static final int BOARD_WIDTH = 300;
 }
